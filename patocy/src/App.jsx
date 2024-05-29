3ade35ff0581e883500405b63e2afb6ba7ba7b2c
Thidas Kaveesha <thidaskaveesha@gmail.com>
2024-05-29 14:18:27 +0530

    reset project

--- patocy/src/App.jsx
+++ patocy/src/App.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Components/Home';
-import OurTeam from './Components/OurTeam';
-import Faq from './Components/Faq';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-import Nav from './Components/Nav';
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav />
-      <Home />
-      <OurTeam />
-      <Faq />
-      <Contact />
-      <Footer />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
